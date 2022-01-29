ad47d327bca2e6c2d2833d3a844499af4f4c95a8
Azerlund6214 <azerlund6214@gmail.com>
2022-01-29 23:33:33 +0300

    Update web.php

--- web.php
+++ web.php
@@ -13,30 +13,84 @@
 
     # /*
 	
-    Route::get('/artisan', function () { echo ' <br>
+    Route::get('/artisan', function ()
+	{
+		
+		
+		
+		
+		
+		
+		echo ' <br>
 			<hr>
-			<h1>EasyArtisan</h1>
+			<h1>~ EasyArtisan ~</h1>
+			<hr>
+            <h2>Общая часть</h2>
 			<style>.box form { display:inline-block; }</style>
 			<style>button { font-size: 16px; padding: 12px; border-radius: 12px; border: 2px solid #4CAF50; /* Green */ }</style>
             <div class="box">
-				<form method="get" action="/artisan-migrate-fresh">  <button type="submit">migrate:fresh --seed</button></form>
-				<form method="get" action="/artisan-migrate">  <button type="submit">migrate</button></form>
+				<form method="get" action="/artisan-migrate-fresh" target="_blank">  <button type="submit">migrate:fresh --seed</button></form>
+				<form method="get" action="/artisan-migrate" target="_blank">  <button type="submit">migrate</button></form>
 			</div>
 			
             <div class="box">
-				<form method="get" action="/artisan-optimize-clear"> <button type="submit">optimize:clear</button></form>
-				<form method="get" action="/artisan-keygen">         <button type="submit">key:generate</button></form>
+				<form method="get" action="/artisan-optimize-clear" target="_blank"> <button type="submit">optimize:clear</button></form>
+				<form method="get" action="/artisan-keygen" target="_blank">         <button type="submit">key:generate</button></form>
 			</div>
-			<hr>
-            <div class="box">
-				<form method="get" action="/artisan-route-list">     <button type="submit">route:list</button></form>
-				<form method="get" action="/artisan-route-list-2">   <button type="submit">route:list --compact</button></form>
+			
+            <div class="box"><br>
+				<form method="get" action="/artisan-route-list" target="_blank">     <button type="submit">route:list</button></form>
+				<form method="get" action="/artisan-route-list-2" target="_blank">   <button type="submit">route:list --compact</button></form>
 			</div>
 			<hr>
 			
-            <span>Миграции</span>
 			<hr>
-		';  }  );
+		';
+		
+		function getModels(): Array
+		{
+			$models = collect(File::allFiles(app_path()))
+				->map(function ($item) {
+					$path = $item->getRelativePathName();
+					$class = sprintf('\%s%s',
+						Container::getInstance()->getNamespace(),
+						strtr(substr($path, 0, strrpos($path, '.')), '/', '\\'));
+
+					return $class;
+				})
+				->filter(function ($class) {
+					$valid = false;
+
+					if (class_exists($class)) {
+						$reflection = new \ReflectionClass($class);
+						$valid = $reflection->isSubclassOf(Model::class) &&
+							!$reflection->isAbstract();
+					}
+
+					return $valid;
+				});
+
+			return $models->values()->toArray();
+		}
+		
+		
+		$modelsInfo = [];
+		foreach(getModels() as $path)
+		{
+			$modelName = explode('\\',$path)[2];
+			$modelsInfo[$modelName]['NAME'] = $modelName;
+			$modelsInfo[$modelName]['NAMESPACE'] = $path;
+			
+			$inst = new $path;
+			$modelsInfo[$modelName]['DB_TABLE'] = $inst->getTable();
+			$modelsInfo[$modelName]['DB_COUNT'] = $inst->count();
+			$modelsInfo[$modelName]['DB_PK']     = $inst->getKeyName();
+			$modelsInfo[$modelName]['DB_PK_TYPE'] = $inst->getKeyType();
+		}
+		
+		dd($modelsInfo);
+		
+		}  );
 		
     Route::get('/artisan-migrate',        function () { Artisan::call('migrate');                           echo "Исполнено => migrate"; dd(Artisan::output());  } );
     Route::get('/artisan-migrate-fresh',  function () { Artisan::call('migrate:fresh', ['--seed' => true]); echo "Исполнено => migrate:fresh --seed"; dd(Artisan::output());  } );
